84e3d9974f85defc785e5580dc32b05a800054dc
Malvi1697 <129746042+Malvi1697@users.noreply.github.com>
2024-05-17 21:04:08 +0200

    Refactored

--- Zookeeper with Python/task/zookeeper/zookeeper.py
+++ Zookeeper with Python/task/zookeeper/zookeeper.py
@@ -1,133 +1,134 @@
-camel = r"""
-Switching on the camera in the camel habitat...
- ___.-''''-.
-/___  @    |
-',,,,.     |         _.'''''''._
-     '     |        /           \
-     |     \    _.-'             \
-     |      '.-'                  '-.
-     |                               ',
-     |                                '',
-      ',,-,                           ':;
-           ',,| ;,,                 ,' ;;
-              ! ; !'',,,',',,,,'!  ;   ;:
-             : ;  ! !       ! ! ;  ;   :;
-             ; ;   ! !      ! !  ; ;   ;,
-            ; ;    ! !     ! !   ; ;
-            ; ;    ! !    ! !     ; ;
-           ;,,      !,!   !,!     ;,;
-           /_I      L_I   L_I     /_I
-Look at that! Our little camel is sunbathing!"""
+def display_habitat(habitat_number):
+    habitats = {
+    "0": r"""
+    Switching on the camera in the camel habitat...
+     ___.-''''-.
+    /___  @    |
+    ',,,,.     |         _.'''''''._
+         '     |        /           \
+         |     \    _.-'             \
+         |      '.-'                  '-.
+         |                               ',
+         |                                '',
+          ',,-,                           ':;
+               ',,| ;,,                 ,' ;;
+                  ! ; !'',,,',',,,,'!  ;   ;:
+                 : ;  ! !       ! ! ;  ;   :;
+                 ; ;   ! !      ! !  ; ;   ;,
+                ; ;    ! !     ! !   ; ;
+                ; ;    ! !    ! !     ; ;
+               ;,,      !,!   !,!     ;,;
+               /_I      L_I   L_I     /_I
+    Look at that! Our little camel is sunbathing!""",
 
-lion = r"""
-Switching on the camera in the lion habitat...
-                                               ,w.
-                                             ,YWMMw  ,M  ,
-                        _.---.._   __..---._.'MMMMMw,wMWmW,
-                   _.-""        '''           YP"WMMMMMMMMMb,
-                .-' __.'                   .'     MMMMW^WMMMM;
-    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
- ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\
-,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
-WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
-"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
-           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
-          /  .'             /  (       .'  /     Ww._     `.  `"
-         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
-        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
-The lion is roaring!"""
+    "1": r"""
+    Switching on the camera in the lion habitat...
+                                                   ,w.
+                                                 ,YWMMw  ,M  ,
+                            _.---.._   __..---._.'MMMMMw,wMWmW,
+                       _.-""        '''           YP"WMMMMMMMMMb,
+                    .-' __.'                   .'     MMMMW^WMMMM;
+        _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+     ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\
+    ,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+    WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+    "^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+               /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+              /  .'             /  (       .'  /     Ww._     `.  `"
+             /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+            (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+    The lion is roaring!""",
 
-deer = r"""
-Switching on the camera in the deer habitat...
-   /|       |\
-`__\\       //__'
-   ||      ||
- \__`\     |'__/
-   `_\\   //_'
-   _.,:---;,._
-   \_:     :_/
-     |@. .@|
-     |     |
-     ,\.-./ \
-     ;;`-'   `---__________-----.-.
-     ;;;                         \_\
-     ';;;                         |
-      ;    |                      ;
-       \   \     \        |      /
-        \_, \    /        \     |\
-          |';|  |,,,,,,,,/ \    \ \_
-          |  |  |           \   /   |
-          \  \  |           |  / \  |
-           | || |           | |   | |
-           | || |           | |   | |
-           | || |           | |   | |
-           |_||_|           |_|   |_|
-          /_//_/           /_/   /_/
-Our 'Bambi' looks hungry. Let's go to feed it!"""
+    "2": r"""
+    Switching on the camera in the deer habitat...
+       /|       |\
+    `__\\       //__'
+       ||      ||
+     \__`\     |'__/
+       `_\\   //_'
+       _.,:---;,._
+       \_:     :_/
+         |@. .@|
+         |     |
+         ,\.-./ \
+         ;;`-'   `---__________-----.-.
+         ;;;                         \_\
+         ';;;                         |
+          ;    |                      ;
+           \   \     \        |      /
+            \_, \    /        \     |\
+              |';|  |,,,,,,,,/ \    \ \_
+              |  |  |           \   /   |
+              \  \  |           |  / \  |
+               | || |           | |   | |
+               | || |           | |   | |
+               | || |           | |   | |
+               |_||_|           |_|   |_|
+              /_//_/           /_/   /_/
+    Our 'Bambi' looks hungry. Let's go to feed it!""",
 
-goose = r"""
-Switching on the camera in the goose habitat...
+    "3": r"""
+    Switching on the camera in the goose habitat...
 
-                                    _
-                                ,-"" "".
-                              ,'  ____  `.
-                            ,'  ,'    `.  `._
-   (`.         _..--.._   ,'  ,'        \    \
-  (`-.\    .-""        ""'   /          (  d _b
- (`._  `-"" ,._             (            `-(   \
- <_  `     (  <`<            \              `-._\
-  <`-       (__< <           :
-   (__        (_<_<          ;
-    `------------------------------------------
-The goose is staring intently at you... Maybe it's time to change the channel?"""
+                                        _
+                                    ,-"" "".
+                                  ,'  ____  `.
+                                ,'  ,'    `.  `._
+       (`.         _..--.._   ,'  ,'        \    \
+      (`-.\    .-""        ""'   /          (  d _b
+     (`._  `-"" ,._             (            `-(   \
+     <_  `     (  <`<            \              `-._\
+      <`-       (__< <           :
+       (__        (_<_<          ;
+        `------------------------------------------
+    The goose is staring intently at you... Maybe it's time to change the channel?""",
 
-bat = r"""
-Switching on the camera in the bat habitat...
-_________________               _________________
- ~-.              \  |\___/|  /              .-~
-     ~-.           \ / o o \ /           .-~
-        >           \\  W  //           <
-       /             /~---~\             \
-      /_            |       |            _\
-         ~-.        |       |        .-~
-            ;        \     /        i
-           /___      /\   /\      ___\
-                ~-. /  \_/  \ .-~
-                   V         V
-This bat looks like it's doing fine."""
+    "4": r"""
+    Switching on the camera in the bat habitat...
+    _________________               _________________
+     ~-.              \  |\___/|  /              .-~
+         ~-.           \ / o o \ /           .-~
+            >           \\  W  //           <
+           /             /~---~\             \
+          /_            |       |            _\
+             ~-.        |       |        .-~
+                ;        \     /        i
+               /___      /\   /\      ___\
+                    ~-. /  \_/  \ .-~
+                       V         V
+    This bat looks like it's doing fine.""",
 
-rabbit = r"""
-Switching on the camera in the rabbit habitat...
-         ,
-        /|      __
-       / |   ,-~ /
-      Y :|  //  /
-      | jj /( .^
-      >-"~"-v"
-     /       Y
-    jo  o    |
-   ( ~T~     j
-    >._-' _./
-   /   "~"  |
-  Y     _,  |
- /| ;-"~ _  l
-/ l/ ,-"~    \
-\//\/      .- \
- Y        /    Y
- l       I     !
- ]\      _\    /"\
-(" ~----( ~   Y.  )
-It looks like we will soon have more rabbits!"""
+    "5": r"""
+    Switching on the camera in the rabbit habitat...
+             ,
+            /|      __
+           / |   ,-~ /
+          Y :|  //  /
+          | jj /( .^
+          >-"~"-v"
+         /       Y
+        jo  o    |
+       ( ~T~     j
+        >._-' _./
+       /   "~"  |
+      Y     _,  |
+     /| ;-"~ _  l
+    / l/ ,-"~    \
+    \//\/      .- \
+     Y        /    Y
+     l       I     !
+     ]\      _\    /"\
+    (" ~----( ~   Y.  )
+    It looks like we will soon have more rabbits!"""
+    }
+    print(habitats.get(habitat_number, "Habitat not found!"))
+def main():
+    while True:
+        user_input = input("Please enter the number of the habitat you would like to view or 'exit' to quit: ")
+        if user_input == "exit":
+            print("See you later!")
+            break
+        display_habitat(user_input)
 
-
-animals = [camel, lion, deer, goose, bat, rabbit]
-
-user_prompt = input("Please enter the number of the habitat you would like to view: ")
-
-while user_prompt != "exit":
-    print(animals[int(user_prompt)])
-    user_prompt = input("Please enter the number of the habitat you would like to view: ")
-else:
-    print("See you later!")
-
-
+if __name__ == "__main__":
+    main()
